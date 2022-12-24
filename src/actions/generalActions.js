ab02e606f016a24296b838427f4159cd89ef7804
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-12-24 22:48:20 +0000

    redux

--- src/actions/generalActions.js
+++ src/actions/generalActions.js
@@ -1,8 +1,10 @@
 export const canDo = (cap, id) => {
   const user = JSON.parse(localStorage.getItem("user"));
+
   if (user && user.capabilities.includes(cap)) {
     return true;
   }
+
   if (id === Number(user && user.id)) {
     return true;
   }
